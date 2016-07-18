b322e6097d949dea6d366d3534bacb275298c328
Wonjun Hwang <hwj1593@naver.com>
2016-07-18 19:34:19 +0900

    the config address is changed

--- configs/linux-x86-demo.c
+++ configs/linux-x86-demo.c
@@ -61,7 +61,7 @@ struct {
 		/* high RAM */ {
 			.phys_start = 0x3bc00000,
 			.virt_start = 0x00800000,
-			.size = 0x3aff000,
+			.size = 0x34ff000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_DMA |
 				JAILHOUSE_MEM_LOADABLE,
